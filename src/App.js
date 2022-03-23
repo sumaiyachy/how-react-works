8e41ce8d7730cdb697e0e0513f141e0115da667f
Sumaiya <sumchymit00@gmail.com>
2022-03-23 18:37:25 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Watch from './components/Watch/Watch';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+     <Watch></Watch>
+    
+    </div>
+  );
+}
+
+export default App;
